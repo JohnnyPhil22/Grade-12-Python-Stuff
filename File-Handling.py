f6d11db52946328e3c6c7bef0f97459f350c839d
Jonathan Philips <johnphilips.995@gmail.com>
2022-05-02 09:33:03 +0400

    Update File-Handling.py

--- File-Handling.py
+++ File-Handling.py
@@ -115,3 +115,85 @@ namefile.flush()
 namefile.close()
 namefile=open('names.txt','r')
 print(namefile.read())
+
+# Count how many times 'a' is present in file
+f=open('blebleble.txt','r')
+data=f.readlines()
+count=0
+for i in data:
+    for j in i:
+        if j=='a':
+            count+=1
+print('Frequency of \'a\' in file:',count)
+
+# Replace 'E' with 'I' while displaying content of file
+f=open('blebleble.txt','r')
+s=f.read().replace('E','I')
+f.close()
+f=open('blebleble.txt','w')
+f.write(s)
+f.flush()
+f=open('blebleble.txt','r')
+print(f.read())
+
+# Display how many uppercase chars and digits are present
+f=open('blebleble.txt','r')
+data=f.readlines()
+countc,countd=0,0
+for i in data:
+    for j in i:
+        if j.isupper():
+            countc+=1
+        if j.isdigit():
+            countd+=1
+print('Number of uppercase characters:',countc,'\nNumber of digits:',countd)
+
+# Display how many vowels are present
+def isvowel(j):
+    return j.upper() in ['A','E','I','O','U']
+f=open('blebleble.txt','r')
+data=f.readlines()
+count=0
+for i in data:
+    for j in i:
+        if isvowel(j):
+            count+=1
+print('Number of vowels:',count)
+
+# Open file to write new content
+f=open('news.txt','w')
+f.write('Hello\nThis is a new file\n')
+f.close()
+# Open file to read existing content
+f=open('news.txt','r')
+print(f.read())
+f.close()
+# Open file to add new content to existing content
+f=open('news.txt','a')
+f.write('News\nMore news\n')
+f.close()
+
+# Read content of file and display occurrence of IS, TO and UP
+def istoupcount():
+    f=open('writer.txt','w')
+    f.write('IT IS UP TO US TO TAKE CARE OF OUR SURROUNDING. IT IS NOT POSSIBLE ONLY FOR THE GOVERNMENT TO TAKE RESPONSIBILITY.')
+    f.close()
+    f=open('writer.txt','r')
+    count=0
+    word=f.read().split()
+    for i in word:
+        if i=='IS' or i=='TO' or i=='UP':
+            count+=1
+    return count
+print('Count of IS, UP and TO:',istoupcount())
+
+# Read lines of file and display those lines starting with A or E
+def countae():
+    with open('writer.txt','w') as f:
+        f.write('A clean environment is necessary for our good health.\nWe should clean our environment.\nEducational Institutes should take the lead.')
+    f=open('writer.txt','r')
+    data=f.readlines()
+    for l in data:
+        if l[0]=='A' or l[0]=='E':
+            print(l)
+countae()
